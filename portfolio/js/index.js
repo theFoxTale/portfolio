4dba6ddaa6fdd5eec29ac7f015cf70f9c5b6af32
Anna Makarenko <makarenkoanna@yandex.ru>
2025-10-01 11:15:01 +0300

    refactor: change portfolio slider behavior

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,7 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
+// Прокручиваем слайдер до середины контента (используется при загрузке страницы)
+function moveSliderToMiddlePosition() {
+    window.addEventListener('DOMContentLoaded', function() {
+        const sliderContainer = document.querySelector('.slider-container');
+        const sliderCarousel = document.querySelector('.slider-carousel');
+
+        const scrollMiddlePosition = (sliderCarousel.scrollWidth + 2 * sliderCarousel.offsetLeft - sliderContainer.clientWidth) / 2;
+
+        sliderContainer.scrollTo({
+            left: scrollMiddlePosition,
+            behavior: 'smooth'
+        });
+    });
+}
+
+// Превращение + в - и обратно в секции FAQ
+function addFaqButtonClicker() {
     document.querySelectorAll('.question-button-container').forEach(item => {
         item.addEventListener('click', function () {
             this.classList.toggle('active');
         });
     });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    addFaqButtonClicker();
+    moveSliderToMiddlePosition();
 });
