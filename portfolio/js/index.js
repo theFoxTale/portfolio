98d726522dc6467fc44bacd6111168ac49873ea3
Anna Makarenko <makarenkoanna@yandex.ru>
2025-10-02 14:01:19 +0300

    fix: carousel center position fix

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,18 +1,3 @@
-// Прокручиваем слайдер до середины контента (используется при загрузке страницы)
-function moveSliderToMiddlePosition() {
-    window.addEventListener('DOMContentLoaded', function() {
-        const sliderContainer = document.querySelector('.slider-container');
-        const sliderCarousel = document.querySelector('.slider-carousel');
-
-        const scrollMiddlePosition = (sliderCarousel.scrollWidth + 2 * sliderCarousel.offsetLeft - sliderContainer.clientWidth) / 2;
-
-        sliderContainer.scrollTo({
-            left: scrollMiddlePosition,
-            behavior: 'smooth'
-        });
-    });
-}
-
 // Превращение + в - и обратно в секции FAQ
 function addFaqButtonClicker() {
     document.querySelectorAll('.question-button-container').forEach(item => {
@@ -24,5 +9,4 @@ function addFaqButtonClicker() {
 
 document.addEventListener('DOMContentLoaded', () => {
     addFaqButtonClicker();
-    moveSliderToMiddlePosition();
 });
