334f86b92791461338d67695936a378a6ee0fe87
Anna Makarenko <makarenkoanna@yandex.ru>
2025-10-02 15:39:22 +0300

    feat: implement accordions in the FAQ section

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,8 +1,22 @@
 // Превращение + в - и обратно в секции FAQ
 function addFaqButtonClicker() {
-    document.querySelectorAll('.question-button-container').forEach(item => {
+    document.querySelectorAll('.question-header').forEach(item => {
         item.addEventListener('click', function () {
-            this.classList.toggle('active');
+            const buttonContainer = this.querySelector('.question-button-container');
+            const isActiveAnswer = buttonContainer.classList.contains('active');
+
+            if (!isActiveAnswer) {
+                const allQuestions = document.querySelectorAll('.question-div');
+                allQuestions.forEach(question => {
+                    question.querySelector('.answer-text').classList.remove('active');
+                    question.querySelector('.question-button-container').classList.remove('active');
+                });
+            }
+
+            buttonContainer.classList.toggle('active');
+
+            const answer = this.nextElementSibling;
+            answer.classList.toggle('active');
         });
     });
 }
