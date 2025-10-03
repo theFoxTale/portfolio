7391ffc7fb53abc7b634c22e9201d7aef68ba8ca
Anna Makarenko <makarenkoanna@yandex.ru>
2025-10-03 05:17:56 +0300

    feat: implement burger menu icon transformation

--- portfolio/js/burger-menu.js
+++ portfolio/js/burger-menu.js
@@ -5,9 +5,12 @@ function toggleBurgerMenu() {
 
     const burgerMenu = document.querySelector('.burger-menu-container');
     burgerMenu.classList.toggle('active');
+
+    const menuIconLines = document.querySelectorAll('.menu-line');
+    menuIconLines.forEach(item => item.classList.toggle('active'));
 }
 
 function addMenuIconClicker() {
     const menuIcon = document.querySelector('.menu-logo-container');
     menuIcon.addEventListener('click', toggleBurgerMenu);
-};
+}
